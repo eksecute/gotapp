430bd547c6860972e57d95cc3378de277b4901df
eksecute <stepan_d@ukr.net>
2020-10-20 10:43:42 +0300

    file upload

---
+++ src/services/gotService.js
@@ -0,0 +1,20 @@
+export default class GotService {
+	constructor() {
+		this._apiBase = 'https://www.anapioficeandfire.com/api';
+	}
+		
+		async getResource(url) {
+			const res = await fetch(`${this._apiBase}${url}`)
+			
+			if (!res.ok) {
+				throw new Error(`Couldn't fetch ${url}, received ${res.status}`)
+			}
+			return await res.json();
+		}
+		getAllCharacters() {
+			return this.getResource(`/characters?page=5&pageSize=10`);
+		}
+		getCharacter(id) {
+			return this.getResource(`/characters/${id}`);
+		}
+}
